09072b3a6d2d02a2ce9c172ea39473da7e55aa1d
fkayoungjo <Jordan.simon55@gmail.com>
2020-07-28 22:39:39 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <div className="header">
+        <h1>Produced By Jordan</h1>
+      </div>
+      <div className="musicPlayer">
+      </div>
+    </div>
+  );
+}
+
+export default App;
